9a3735247b81124daf624a601357f7dd7b499b07
unknown <my@547284607.com>
2018-04-10 18:28:12 +0800

    f

--- js/table.js
+++ js/table.js
@@ -1,29 +1,38 @@
-//选项卡的实现思路，鼠标滑到某一个li上面，让当前的li和对应的div有select样式，让其他没有这个样式
 
-//分析需求和思路
-
-//要操作谁就先获取谁
-
-var oTab =document.getElementById("tab");
-var oLis=oTab.getElementsByTagName("li");
-var oDivs=oTab.getElementsByTagName("div");
-
-//实现一个功能方法
-function tabChange(nindex) {
-    for (var i = 0; i < oLis.length; i++) {
-        oLis[i].className = "";
-        oDivs[i].className = "";
-        console.log("ok");
+    //获取元素
+   /*var oTab = document.getElementById("tab");
+    var oLis = oTab.getElementsByTagName("li");
+    var oDivs = oTab.getElementsByTagName("div");
+    
+ function tabChange(n){
+     for (var i=0;i<oLis.length;i++){
+         oLis[i].className="";
+         oDivs[i].className="";
+     }
+     oLis[n].className="select";
+     oDivs[n].className="select";
+ }
+for (var i=0;i<oLis.length;i++)
+{  oLis[i].lilun=i;
+    oLis[i].onmouseover=function()
+    {
+        tabChange(this.lilun);
     }
-    oLis[nindex].className = "select";
-    oDivs[nindex].className = 'select';  
-}
-//给我们的li绑定鼠标移上去的事件
-for (var i = 0; i < oLis.length; i++) {
-    oLis[i].lilun = i;
-    oLis[i].onmouseover = function () {  
-        tabChange(this.lilun);     
-    }
+}*/
 
-}
-//index是我们定义的形参，代表当前被选中元素的索引
+    var oTab = document.getElementById("tab");
+    var oLis = oTab.getElementsByTagName("li");
+    var oDivs = oTab.getElementsByTagName("div");
+
+    for(var i=0;i<oLis.length;i++){
+        var oLi =oLis[i]
+        oLi.lilun = i;
+        oLi.onmouseover=function(){
+            for(var j=0;j<oLis.length;j++){
+                oLis[j].className="";
+                oDivs[j].className="";
+            }
+            this.className="select";
+            oDivs[this.lilun].className="select";
+        }
+    }
